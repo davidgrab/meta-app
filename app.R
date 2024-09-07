148f99adfeb629eef68c285fa0ba17975b7c32e3
davidgrabois <davidgrabois@gmail.com>
2024-09-07 19:53:06 +0300

    add circel config

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 # Modern Meta-Analysis Shiny App
 # This app provides tools for conducting and visualizing meta-analyses.
 # Run this app using: shiny::runApp()
+# Run tests testthat::test_dir("tests/testthat")
 
 source("R/ui.R")
 source("R/server.R")
