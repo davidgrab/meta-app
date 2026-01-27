dde31c602e6118a19a128df279a3064608ebcfd0
davidgrabois <davidgrabois@gmail.com>
2026-01-27 22:30:10 +0200

    fix

---
+++ _dependencies.R
@@ -0,0 +1,16 @@
+# This file is used to hint rsconnect about dependencies that might not be
+# automatically detected in the source code.
+# It is not used by the app itself.
+
+library(metarep)
+library(remotes)
+library(meta)
+library(metafor)
+library(ggplot2)
+library(plotly)
+library(DT)
+library(bslib)
+library(bsicons)
+library(shinyjs)
+library(shinycssloaders)
+library(jsonlite)
